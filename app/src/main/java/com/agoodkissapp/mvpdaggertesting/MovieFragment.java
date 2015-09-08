6873acdf795afa7a089ba37edfe0fb5a5f804547
jtizzle <jh011889@gmail.com>
2015-09-07 21:24:50 -0400

    switched to cardview for movie_row

--- app/src/main/java/com/agoodkissapp/mvpdaggertesting/MovieFragment.java
+++ app/src/main/java/com/agoodkissapp/mvpdaggertesting/MovieFragment.java
@@ -4,6 +4,7 @@ package com.agoodkissapp.mvpdaggertesting;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
