d0e63cd576fedfc07405b80566108ecfde16a114
jtizzle <jh011889@gmail.com>
2015-09-29 01:14:00 -0400

    selector for cardview now in foreground

--- app/src/main/java/com/agoodkissapp/mvpdaggertesting/Adapters/RecyclerAdapter.java
+++ app/src/main/java/com/agoodkissapp/mvpdaggertesting/Adapters/RecyclerAdapter.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.agoodkissapp.mvpdaggertesting.Movie;
 
@@ -66,7 +67,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         holder.setClickListener(new ClickListener() {
             @Override
             public void onClick(View v, int position) {
-                //Toast.makeText(context, movieList.get(position).title, Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, movieList.get(position).title, Toast.LENGTH_SHORT).show();
             }
         });
 
